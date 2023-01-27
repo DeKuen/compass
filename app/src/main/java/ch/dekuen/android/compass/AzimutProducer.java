671187606a096f05c53306f960a3483b0aa608e7
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2023-01-28 00:54:38 +0100

    Revert "Use Future to call listeners"
    This reverts commit 2a3bceb657c24d362fd31754fe627d9f7be6371f.

--- app/src/main/java/ch/dekuen/android/compass/AzimutProducer.java
+++ app/src/main/java/ch/dekuen/android/compass/AzimutProducer.java
@@ -2,20 +2,15 @@ package ch.dekuen.android.compass;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public abstract class AzimutProducer {
     private final List<AzimutListener> listeners = new ArrayList<>();
-    private final ExecutorService executor = Executors.newSingleThreadExecutor();
-
 
     public final void addListener(AzimutListener listener) {
         listeners.add(listener);
     }
 
     protected final void callListeners(float azimut) {
-        listeners.parallelStream()
-                .forEach(listener -> executor.submit(() -> listener.onNewAzimut(azimut)));
+        listeners.forEach(listener -> listener.onNewAzimut(azimut));
     }
 }
