4fb2a5201b46bc759dee3a32877f730908897fa7
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2023-01-29 13:45:08 +0100

    Refactor CompassSensorEventListener into separate classes

--- app/src/main/java/ch/dekuen/android/compass/sensor/CompassSensorEventListener.java
+++ app/src/main/java/ch/dekuen/android/compass/sensor/CompassSensorEventListener.java
@@ -3,20 +3,17 @@ package ch.dekuen.android.compass.sensor;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.util.Log;
 
-import ch.dekuen.android.compass.AzimutListener;
+import java.util.function.Consumer;
 
 public class CompassSensorEventListener implements SensorEventListener {
-    static final float LOW_PASS_FILTER_ALPHA = 0.97f;
-    private final AzimutListener listener;
-    private float[] accelerationMeasurements;
-    private float[] magneticMeasurements;
-    private float azimut;
+    private final Consumer<float[]> accelerationConsumer;
+    private final Consumer<float[]> magneticConsumer;
 
-    public CompassSensorEventListener(AzimutListener listener) {
-        this.listener = listener;
+    public CompassSensorEventListener(Consumer<float[]> accelerationConsumer, Consumer<float[]> magneticConsumer) {
+        this.accelerationConsumer = accelerationConsumer;
+        this.magneticConsumer = magneticConsumer;
     }
 
     @Override
@@ -32,36 +29,12 @@ public class CompassSensorEventListener implements SensorEventListener {
         }
         int sensorType = sensor.getType();
         if (sensorType == Sensor.TYPE_ACCELEROMETER) {
-            accelerationMeasurements = event.values;
+            accelerationConsumer.accept(event.values);
         } else if (sensorType == Sensor.TYPE_MAGNETIC_FIELD) {
-            magneticMeasurements = event.values;
+            magneticConsumer.accept(event.values);
         } else {
             Log.e(getClass().getName(), "Unexpected sensor type");
-            return;
         }
-        if (accelerationMeasurements == null) {
-            Log.i(getClass().getName(), "accelerationMeasurements is still null");
-            return;
-        } else if (magneticMeasurements == null) {
-            Log.i(getClass().getName(), "magneticMeasurements is still null");
-            return;
-        }
-        boolean isPhoneFacingUp = accelerationMeasurements[2] >= 0;
-        float[] matrixR = new float[9];
-        boolean success = SensorManager.getRotationMatrix(matrixR, null, accelerationMeasurements, magneticMeasurements);
-        if (!success) {
-            Log.i(getClass().getName(), "could not calculate rotation matrix");
-            return;
-        }
-        float[] orientation = new float[3];
-        // orientation contains: azimut, pitch and roll
-        SensorManager.getOrientation(matrixR, orientation);
-        // get angle around the z-axis rotated
-        float azimutRaw = orientation[0];
-        // apply low pass filter
-        azimut = LOW_PASS_FILTER_ALPHA * azimut + (1 - LOW_PASS_FILTER_ALPHA) * azimutRaw;
-        // send to listener
-        listener.onNewAzimut(azimut, isPhoneFacingUp);
     }
 
     @Override
